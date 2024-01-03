0fdc6fed00c41bc482255adb9a32dfdaa9c33001
MMK <staph103@gmail.com>
2024-01-03 13:02:13 -0500

    working on switch cases and importing mysql

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 const fs = require('fs')
 const generateMarkdown = require('./utils/mdGenerator')
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password
+        password: 'rootroot',
+        database: 'directory_db'
+    },
+    console.log(`Connected to the books_db database.`)
+);
+
 
 inquirer
     .prompt([
@@ -11,8 +27,58 @@ inquirer
             type: 'list',
             name: 'departments',
             message: 'Welcome to MMK LLC',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role','Add an employee','Update an employee role']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
         },
 
-    ])
+    ]).then((res) => {
+        let newShape;
+        //
+        switch (res.choices) {
+            case 'View all departments':
 
+                db.query(`SELECT * FROM departments `, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(result);
+                });
+
+                break;
+
+            case 'View all roles':
+
+                db.query(`SELECT * FROM roles `, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(result);
+                })
+
+                    break;
+
+            case 'View all employees':
+
+                db.query(`SELECT * FROM employees `, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(result);
+                })
+                break;
+
+        }
+    })
+
+//creates the svg file based on data retrieved from user input
+
+db.query(`SELECT * FROM departments `, function (err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// Query database
+db.query('SELECT * FROM course_names', function (err, results) {
+    console.log(results);
+});
---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS directory_db;
+CREATE DATABASE directory_db;
+
+USE directory_db;
+
+CREATE TABLE employees (
+
+
+)
+
+CREATE TABLE roles (
+
+
+)
+
+CREATE TABLE departments (
+
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+
+);
+
+
